fdd9ca452589cd5fc9025d3a81032437b3a8f10e
Nikola Zorić <nikola.se.zoric@gmail.com>
2021-05-09 21:57:56 +0200

    Configured tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  prefix: "",
+  purge: {
+    enabled: false,
+    content: ["./src/**/*.{html,ts}"],
+  },
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      backgroundColor: ['active']
+    },
+  },
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
+};
